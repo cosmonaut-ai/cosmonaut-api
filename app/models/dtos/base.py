bf06a4de6f740dd327d94c48a959f54536efce59
Ian <imatson9119@gmail.com>
2025-12-29 12:07:21 -0600

    Dummy commit

--- app/models/dtos/base.py
+++ app/models/dtos/base.py
@@ -10,6 +10,6 @@ DTO = TypeVar("DTO", bound=BaseModel)
 
 
 class DTOModel(BaseModel):
-  """Base DTO with sensible defaults for API respon ses."""
+  """Base DTO with sensible defaults for API responses."""
 
   model_config = ConfigDict(populate_by_name=True)
