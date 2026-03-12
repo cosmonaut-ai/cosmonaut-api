9d5016b10fc6acca7545d883a10428d3cdff696a
Ian <imatson9119@gmail.com>
2026-03-12 16:11:03 -0500

    Refactor projection attributes in GSI2Model for story nodes
    - Simplified the projection in the GSI2Model by removing the `node_generation_status` attribute and updating the `generation_status` attribute, streamlining data retrieval for story nodes.

--- app/models/entities/story_node.py
+++ app/models/entities/story_node.py
@@ -25,9 +25,7 @@ class GSI2Model(GlobalSecondaryIndex):  # type: ignore[type-arg]
   GSI2SK: UnicodeAttribute = UnicodeAttribute(range_key=True)
 
   class Meta:  # type: ignore[misc]
-    projection = IncludeProjection(
-      ["node_title", "node_choices", "node_parent_id", "node_generation_status", "node_id"]
-    )
+    projection = IncludeProjection(["node_title", "node_choices", "node_parent_id", "generation_status", "node_id"])
 
 
 class ChoiceMap(MapAttribute[str, UnicodeAttribute]):
